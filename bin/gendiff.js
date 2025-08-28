10c859ec7613db0e4bdc4589120749d4e12da5b4
1manaliyev <baxtik.kkp@mail.ru>
2025-08-28 15:12:51 +0500

    step 3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from "commander";
+import genDiff from "../src/index.js";
 
 const program = new Command();
 
@@ -10,10 +11,8 @@ program
   .arguments("<filepath1> <filepath2>")
   .option("-f, --format [type]", "output format")
   .action((filepath1, filepath2, options) => {
-    // пока просто выводим полученные данные
-    console.log("File 1:", filepath1);
-    console.log("File 2:", filepath2);
-    console.log("Format:", options.format);
+    const diff = genDiff(filepath1, filepath2);
+    console.log("Result:", diff);
   });
 
 program.parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
+import fs from "fs";
+import path from "path";
+
+const getFileData = (filepath) => {
+  const fullPath = path.resolve(process.cwd(), filepath); // абсолютный путь
+  const data = fs.readFileSync(fullPath, "utf-8"); // читаем содержимое
+  return JSON.parse(data); // парсим JSON
+};
+
 const genDiff = (filepath1, filepath2) => {
-  return "diff result";
+  const data1 = getFileData(filepath1);
+  const data2 = getFileData(filepath2);
+
+  // пока просто выводим оба объекта (позже сделаем красивый diff)
+  return { data1, data2 };
 };
 
 export default genDiff;
