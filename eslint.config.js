e0192427a8452067c21066534f3554d64f339768
1manaliyev <baxtik.kkp@mail.ru>
2025-08-28 20:42:53 +0500

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,16 @@ import pluginJest from "eslint-plugin-jest";
 import globals from "globals";
 
 export default [
+  {
+    ignores: [
+      "__tests__/**",
+      "coverage/**",
+      "bin/**",
+      // если по какой-то причине останутся служебные файлы в code/,
+      // тоже игнорируем всю папку:
+      "code/**",
+    ],
+  },
   js.configs.recommended,
   {
     files: ["**/*.{js,mjs,cjs}"],
